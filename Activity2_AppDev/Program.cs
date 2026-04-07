8768b8a15d44826848fb5713203ef9b09720ffd5
luis-sss <luisgabrieldelacruz00@gmail.com>
2026-04-07 09:44:42 +0800

    Add project files.

---
+++ Activity2_AppDev/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ");
+            Console.WriteLine("  Hi!, I'm Luis Gabriel F. Dela Cruz and this is my assignment for Activity#2");
+            Console.WriteLine("^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ");
+
+        }
+    }
+}
