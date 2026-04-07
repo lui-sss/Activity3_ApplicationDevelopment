6e257f123cc40bce33891f8fec0ace5d724d5bf6
Luis Gabriel F. Dela Cruz <luisgabrieldelacruz00@gmail.com>
2026-04-07 14:50:47 +0800

    Pulling Line 13

--- Activity2_AppDev/Program.cs
+++ Activity2_AppDev/Program.cs
@@ -10,6 +10,7 @@ namespace MyProject
             Console.WriteLine("  Hi!, I'm Luis Gabriel F. Dela Cruz and this is my assignment for Activity#2");
             Console.WriteLine("^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ^v^ ");
 
+            Console.WriteLine(" This is my assignment for Activity 3 "); // Pull
         }
     }
 }
